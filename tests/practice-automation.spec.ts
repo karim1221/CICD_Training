c6fa7047962318b79faa95c4d0d47b63f48f950d
karim1221 <karim_1221@icloud.com>
2026-03-26 15:52:45 -0600

    test(ci): run workflow test

--- tests/practice-automation.spec.ts
+++ tests/practice-automation.spec.ts
@@ -6,7 +6,7 @@ test.describe('Practice Automation', () => {
     await page.waitForLoadState('domcontentloaded');
   });
 
-  test('should display the correct page title on the home page', async ({ page }) => {
+  test('should display the correct page title on the home page:', async ({ page }) => {
     await page.screenshot({
       path: 'test-results/title_validation.png',
       fullPage: true,
